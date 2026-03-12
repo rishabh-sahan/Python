51f82b3e5ac0b7df92d0e6da2ab789112546826d
Rishabh Jain <rishabhsahan@gmail.com>
2026-03-12 23:46:56 +0530

    py basics

---
+++ code.py
@@ -0,0 +1,26 @@
+# print("Hello, World!");
+# print("Welcome to coding!");
+
+# print("Hello, World!", "Welcome to coding!");
+
+# print("Hello, \nWorld!", "Welcome to coding!");
+
+# Identifiers
+name = "Rishabh"
+_name = "Jain"
+age = 20
+PI = 3.14
+Bool = True
+NoneType = None
+
+# print(name, age, PI)
+# print("My Name is:", name)
+# print("My Age is:", age+1) # we can also do operations in print statement
+# print("My Name is: ", name, _name) # we can also print multiple variables in a single print statement
+
+# Data Types
+print(type(name)) # str
+print(type(age)) # int 
+print(type(PI)) # float
+print(type(Bool)) # bool
+print(type(NoneType)) # NoneType
