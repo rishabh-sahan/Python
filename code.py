4a9a5537bf6b0c45cb6eeaa45c014250896d21aa
Rishabh Jain <rishabhsahan@gmail.com>
2026-03-13 23:19:36 +0530

    py basics

--- code.py
+++ code.py
@@ -37,7 +37,28 @@ comment
 # TotPrice = 100 # PascalCase
 
 # WAP to calculate sum of two numbers
-a = 5
-b = 10
-sum = a + b
-print(sum)
+# a = 5
+# b = 10
+# sum = a + b
+# print(sum)
+
+# Arithmetic Operators
+# a = 10
+# b = 5
+# print(a+b) # Addition
+# print(a-b) # Subtraction
+# print(a*b) # Multiplication
+# print(a/b) # Division
+# print(a%b) # Modulus
+# print(a**b) # Exponentiation
+
+# Relational or Comparison Operators
+# a = 10
+# b = 5
+# print(a == b) # Equal to
+# print(a != b) # Not equal to
+# print(a > b) # Greater than
+# print(a < b) # Less than
+# print(a >= b) # Greater than or equal to
+# print(a <= b) # Less than or equal to
+
