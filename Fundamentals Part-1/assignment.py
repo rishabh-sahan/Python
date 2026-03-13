0a39cfda67f4cba6c2dfefad650081081e3c5428
Rishabh Jain <rishabhsahan@gmail.com>
2026-03-14 01:31:10 +0530

    Completed Fundamentals Part-1

---
+++ Fundamentals Part-1/assignment.py
@@ -0,0 +1,71 @@
+# Q1. Write a program that asks the user for their name and age, then prints a sentence like;
+# Hello Shradha, you are 21 years old!
+# name=input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello", name + ", you are " + age + " years old!") 
+
+# Q2. Take two numbers as input from the user and print their sum, difference, product, and quotient.
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+# print("Sum:", a + b)
+# print("Difference:", a - b)
+# print("Product:", a * b)
+# print("Quotient:", a / b)
+
+# Q3. Ask the user to enter two integers and one float. Convert them all to floats and print their average.
+# int1 = int(input("Enter the 1st integer value: "))
+# int2 = int(input("Enter the 2nd integer value: "))
+# float1 = float(input("Enter the float value: "))
+# avg = float(int1 + int2 + float1)/3
+# print(avg)
+
+# Q4. The user enters a string containing a number (eg: "45"). Convert it to:
+# an integer, a float, a string again
+# Print all three values with their types.
+# string = input("Enter the number: ")
+# integer = int(string)
+# print(integer, type(integer))
+# floatval = float(string)
+# print(floatval, type(floatval))
+# stringval = str(string)
+# print(stringval, type(stringval))
+
+#Q5. 
+# x = 10 + 3 * 2 ** 2
+# print(x)
+
+# Q6. Write a program to swap values of two numbers entered by the user.
+# a = int(input("Enter the value of a: "))
+# b = int(input("Enter the value of b: "))
+# temp=a
+# a=b
+# b=temp
+# print("After Swapping")
+# print("a = ", a)
+# print("b = ", b)
+
+# Q7. Ask the user for a temperature in Celsius (string input). Conert it to float, then calculate and print temperature in Fahrenheit.
+# celTemp = input("Enter the temperature in celsius: ")
+# floatval = float(celTemp)
+# fahrenTemp = float(floatval*(9/5))+32
+# print("Temperature in Fahrenheit:", fahrenTemp)
+
+# Q8. Take the radius (r) as user input and print the area
+# PI = 3.14
+# radius = float(input("Enter the radius: "))
+# area = PI * radius * radius
+# print("Area =", area)
+
+# Q9. Ask the user for: Principal (P), Rate (R), Time(T). Convert all to float and compute simple interest
+# P = float(input("Enter the Principal: "))
+# R = float(input("Enter the Rate: "))
+# T = float(input("Enter the Float: "))
+# SI = (P * R * T) / 100
+# print(SI)
+
+# Q10. Take a decimal number as input (like 45.78) and ouput is integer part - 45, fractional part - .78
+# num = float(input("Enter a decimal number: "))
+# integer_part = int(num)
+# fractional_part = num - integer_part
+# print("Integer part =", integer_part)
+# print("Fractional part =", fractional_part)
