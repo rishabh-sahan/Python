f62468921f96d88f33d9b003472654de6e85f22c
Rishabh Jain <rishabhsahan@gmail.com>
2026-03-15 00:09:23 +0530

    Updated Fundmentals Part-2

---
+++ Fundamentals Part-2/code.py
@@ -0,0 +1,47 @@
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You can vote!")
+#     print("You can drive!")
+# else:
+#     print("You can't vote")
+#     print("You can't drive")
+
+# color = input("Enter a color: ")
+# if color == "red" or color == "Red":
+#     print("Stop!")
+# elif color == "yellow":
+#     print("Get ready!")
+# elif color == "green":
+#     print("Go!")
+# else:
+#     print("Wrong color for traffic lights!")
+
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child.")
+# elif age >= 13 and age < 18:
+#     print("You are a teenager.")
+# else:
+#     print("You are an adult.")
+
+# username = input("Enter your username: ")
+# password = input("Enter your password: ")
+# if (username == "admin" and password == "pass"):
+#     print("Login Successfull!!")
+# elif (username != "admin"):
+#     print("Wrong Username")
+# else:
+#     print("Wrong Password")
+
+# n = int(input("Enter the number: "))
+# if n % 5 == 0:
+#     print(n, "is multiple of 5")
+# else:
+#     print(n, "is not multiple by 5")
+
+# num = int(input("Enter the number: "))
+# if (num % 2 == 0):
+#     print(num, "is even number")
+# else:
+#     print(num, "is odd number")
+
